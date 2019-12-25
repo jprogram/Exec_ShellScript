5456b17cc884c24ec7bf539ea99cb0a8324b5ccd
Jefferson Gomes <jeffsilvaygo@gmail.com>
2019-12-25 21:22:22 -0200

    Complemento das opcoes

--- OperacoesValores.sh
+++ OperacoesValores.sh
@@ -3,10 +3,12 @@
 
 #!/bin/bash
 
+clear
+
 read -p "Informe o Valor1: " VALOR1
 read -p "Informe o Valor2: " VALOR2
 
-echo -e "Escolha uma Operação: "
+echo -e "\nEscolha uma Operação: "
 echo -e "1 = Soma"
 echo -e "2 = Subtração"
 echo -e "3 = Multiplicação"
@@ -18,11 +20,30 @@ read -p "Opção: " OPCAO
 case $OPCAO in
 	1)
 		SOMA=$(($VALOR1+$VALOR2))
-		echo "O Resultado da soma eh $SOMA"
+		echo -e "\nO Resultado da soma eh $SOMA"
 		;;
 		
 	2)
 		SUBTRACAO=$(($VALOR1-$VALOR2))
-		echo "O Resultado da subtracao eh $SUBTRACAO"
+		echo -e "\nO Resultado da subtracao eh $SUBTRACAO"
 		;;
+		
+	3)
+		MULTIPLICACAO=$(($VALOR1*$VALOR2))
+		echo -e "\nO Resultado da subtracao eh $MULTIPLICACAO"
+		;;
+		
+	4)
+		DIVISAO=$(($VALOR1/$VALOR2))
+		echo -e "\nO Resultado da subtracao eh $DIVISAO"
+		;;
+		
+	Q | q)
+		echo -e "\nSaindo do programa!"
+		;;
+		
+	*)
+		echo -e "\nOpção Inválida!"
+		;;		
+	
 esac		
